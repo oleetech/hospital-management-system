81b98e3370d42cb11db22aecc54a08894e72d04b
Olee Ahmmed Ashik <124195105+oleetech@users.noreply.github.com>
2024-02-19 21:27:39 +0600

    add Nurse Model

--- Doctor/admin.py
+++ Doctor/admin.py
@@ -1,10 +1,14 @@
 
 from django.contrib import admin
-from .models import Doctor, DoctorAvailability
+from .models import Doctor, DoctorAvailability,Nurse
 
 class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'gender', 'contact_number', 'email', 'specialization', 'qualifications')
+    list_display = ('name', 'gender', 'contact_number', 'specialization', 'qualifications')
     search_fields = ['name', 'specialization']  # Add fields you want to search by
+
+class NurseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gender')
+    search_fields = ['name' ]  
 
 class DoctorAvailabilityAdmin(admin.ModelAdmin):
     list_display = ('doctor', 'day_of_week', 'start_time', 'end_time')
@@ -12,3 +16,4 @@ class DoctorAvailabilityAdmin(admin.ModelAdmin):
 
 admin.site.register(Doctor, DoctorAdmin)
 admin.site.register(DoctorAvailability, DoctorAvailabilityAdmin)
+admin.site.register(Nurse, NurseAdmin)
